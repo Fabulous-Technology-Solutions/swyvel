66572994d1a73d2793ef577e2f2e8e27007c3b41
Muhammad Umair Nizam <muhammadumairnizam@fabtechsol.com>
2025-03-22 16:35:40 +0500

    layout structure

---
+++ src/pages/landingPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center ">
+    <div class=" text-center">
+      <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-vertical.svg"
+        style="width: 200px; height: 200px"
+      />
+      <h1 class="text-center">landing page</h1>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+//
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,16 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: '/dashboard',
+    component: () => import('src/layouts/DashboardLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+  {
+    path: '/',
+    component: () => import('src/layouts/LandingLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/landingPage.vue') }
     ]
   },
 
