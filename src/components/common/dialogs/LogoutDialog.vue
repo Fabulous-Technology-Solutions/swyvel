2a8d7db356ae023fb3f9d12abc4428719151d350
WaseemIrshadFab <waseem.irshad@fabtechsol.com>
2025-03-25 00:35:10 +0500

    Create Post and Logout Dilaog Updated

--- src/components/common/dialogs/LogoutDialog.vue
+++ src/components/common/dialogs/LogoutDialog.vue
@@ -3,15 +3,15 @@
       <q-card style="width: 400px; border-radius: 12px">
         <q-card-section class="q-gutter-y-lg">
           <div>
-            <div class="text-h6 text-bold q-mb-sm">Delete</div>
+            <div class="text-h6 text-bold q-mb-sm">Log Out</div>
             <div>
-              Are you sure you want to delete this? This action cannot be undone.
+              Are you sure you want to log out your profile?
             </div>
           </div>
 
           <div class="flex justify-between items-center q-gutter-x-md">
             <q-btn @click="hideDialog" no-caps flat label="Cancel" class="col border rounded-8 q-py-sm" />
-            <q-btn no-caps unelevated label="Delete" color="negative" class="col rounded-8 q-py-sm" />
+            <q-btn no-caps unelevated label="Log Out" color="negative" class="col rounded-8 q-py-sm" />
           </div>
         </q-card-section>
       </q-card>
